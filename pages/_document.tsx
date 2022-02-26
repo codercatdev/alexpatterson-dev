d92c536e9a65da196acadc824bc532febc2cfa6f
Alex Patterson <alex@codingcat.dev>
2022-02-26 10:19:06 -0500

    add card with photo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html data-theme="synthwave">
       <Head>
-      <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,19 +17,33 @@ const Home: NextPage = () => {
           site_name: 'Alex Patterson',
           images: [
             {
-              url: `https://media.codingcat.dev/image/upload/c_fit,w_1200,h_630/authors/alex_headshot`,
+              url: `https://media.codingcat.dev/image/upload/c_fit,w_1200,h_630/main-codingcatdev-photo/authors/alex_headshot`,
               width: 1200,
               height: 630,
               alt: 'Alex patterson',
             },
             {
-              url: `https://media.codingcat.dev/image/upload/authors/alex_headshot`,
+              url: `https://media.codingcat.dev/image/upload/main-codingcatdev-photo/authors/alex_headshot`,
               alt: 'Alex patterson',
             },
           ],
         }}
       ></NextSeo>
-      Hello World
+      <div className="card w-96 bg-primary text-primary-content">
+        <div className="card-body">
+          <h2 className="card-title">Alex Patterson</h2>
+          <div className="avatar">
+            <div className="w-full">
+              <Image
+                src="/main-codingcatdev-photo/authors/alex_headshot"
+                alt="Headshot of Alex Patterson"
+                layout="fill"
+                className="rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
