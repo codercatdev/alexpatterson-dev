716324c0ed972082da4c3471e441a15125903914
Alex Patterson <alex@codingcat.dev>
2022-02-26 10:02:44 -0500

    add seo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'cloudinary',
+    path: 'https://media.codingcat.dev/image/upload/',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,29 @@ const Home: NextPage = () => {
   return (
     <>
       <NextSeo
-        title="Simple Usage Example"
-        description="A short description goes here."
-      />
+        title="Alex Patterson"
+        description="Developer Advocate"
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: `https://alexpatterson.dev`,
+          title: 'Alex Patterson',
+          description: 'Developer Advocate',
+          site_name: 'Alex Patterson',
+          images: [
+            {
+              url: `https://media.codingcat.dev/image/upload/c_fit,w_1200,h_630/authors/alex_headshot`,
+              width: 1200,
+              height: 630,
+              alt: 'Alex patterson',
+            },
+            {
+              url: `https://media.codingcat.dev/image/upload/authors/alex_headshot`,
+              alt: 'Alex patterson',
+            },
+          ],
+        }}
+      ></NextSeo>
       Hello World
     </>
   );
